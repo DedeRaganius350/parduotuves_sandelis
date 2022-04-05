caf41de7900a2b3af5ee3b6f7db6b02e042978b6
edvin <edvinas3500@gmail.com>
2022-04-05 21:24:02 +0300

    index.php faile sukuriau forma "Prisijungimas"

--- index.php
+++ index.php
@@ -1,8 +1,39 @@
 <?php
 
-echo 'veikia';
-echo 'sveika';
-echo 'sveikas';
-echo 'labass';
-echo 'labutis';
-echo 'labute';
+/**
+ * prisijungimas
+ * registracija su rolemis (sandelio darbuotojas, parduotuves darbuotojas)
+ *
+ * //sandelio darbuotojas
+ * prekiu kategorijos (duona, pieno produktai, kaceliarija ... )
+ * prekiu valdymas (prideti, atnaujinti, istrinti) - prekės yra priskirtos prie parduotuvės
+ * sandėlis - jame saugomos visos prekiu sarasas ir yra maksimalus vienetu skaicius
+ *
+ * parduotuviu uzsakymai is sandelio (sandelio istorija)
+ *
+ * //parduotuves darbuotojas
+ * parduotuviu valdymas (prideti, atnaujinti, istrinti) (bendra marzas, papildomos marzos pagal kategorijas arba akcijos)
+ * parduotuves prekiu informacija (galiojimo laikas, kiekis/vienetai, kaina
+ * prekes kurias reikejo ismesti (kuriu galiojimas baigiasi) parduotuve
+ *
+ * pirkejas ir jo krepselis(kur ir ka pirko ir kiek sumokejo)
+ *
+ * statistika (kiek parduotuves uzdirbo, kokios prekes populiariausios)
+ */
+
+?>
+
+<h1>Parduotuve Sandelis</h1>
+
+<form action="index.php">
+    <fieldset>
+        <legend>Prisijungimas:</legend>
+        <label for="vardas">Vardas:</label>
+        <input type="text" id="vardas" name="vardas"><br><br>
+        <label for="slaptazodis">Slaptazodis:</label>
+        <input type="text" id="slaptazodis" name="slaptazodis"><br><br>
+        <input type="submit" value="Prisijungti">
+        <hr>
+        <a href="#">Registruotis</a>
+    </fieldset>
+</form>
